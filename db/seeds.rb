bcf13869f308460a227b9db4850769eec00ba4b7
WizardofHause <jonrhause@gmail.com>
2022-12-14 15:57:11 -0700

    making seeds, committing for safety sake

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,73 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 puts "planting seeds 🌱"
+
 User.destroy_all
+Memory.destroy_all
+Comment.destroy_all
 
 puts "Creating Test User"
-User.create(name: 'Test', email: 'Test', password: '1111')
+u1 = User.create(name: 'Test', email: 'Test', password: '1111')
+u2 = User.create(name: "Jon", email: "jon@email.com", password: "abc123")
+u3 = User.create(name: "Melody", email: "melody@email.com", password: "mel123")
+u4 = User.create(name: "Sam", email: "sam@email.com", password: "sam123")
+u5 = User.create(name: "Tim", email: "tim@email.com", password: "tim123")
+u6 = User.create(name: "Julie", email: "julie@email.com", password: "jul123")
+u7 = User.create(name: "Jess", email: "jess@email.com", password: "jes123")
+u8 = User.create(name: "Joe", email: "joe@email.com", password: "joe123")
+u9 = User.create(name: "Lucy", email: "lucy@email.com", password: "luc123")
+u10 = User.create(name: "Stella", email: "stella@email.com", password: "ste123")
+u11 = User.create(name: "Abbie", email: "abbie@email.com", password: "abb123")
+u12 = User.create(name: "Tobey", email: "tobey@email.com", password: "tob123")
+u13 = User.create(name: "August", email: "august@email.com", password: "aug123")
+u14 = User.create(name: "Judy", email: "judy@email.com", password: "jud123")
+
+puts "Making Memories..."
+m1 = Memory.create(title: "Trip to the Dunes", type: "vacation", status: "completed", main_img: "https://www.nps.gov/common/uploads/grid_builder/grsa/crop16_9/2E8879CA-EF95-283E-C88B7FE90F149C36.jpg?width=950&quality=90&mode=crop", description: "Mel and I went to the Sand Dunes and had a lovely time.", likes: 5, user_id: u2 )
+
+puts "Generating Comments..."
+Comment.create(user_id: u1, memory_id: m1, body: "What a great trip!")
+Comment.create(user_id: u1, memory_id: m2, body: "")
+Comment.create(user_id: u1, memory_id: m3, body: "")
+Comment.create(user_id: u2, memory_id: m4, body: "")
+Comment.create(user_id: u2, memory_id: m5, body: "")
+Comment.create(user_id: u2, memory_id: m6, body: "")
+Comment.create(user_id: u3, memory_id: m7, body: "")
+Comment.create(user_id: u3, memory_id: m8, body: "")
+Comment.create(user_id: u3, memory_id: m9, body: "")
+Comment.create(user_id: u4, memory_id: m10, body: "")
+Comment.create(user_id: u4, memory_id: , body: "")
+Comment.create(user_id: u4, memory_id: , body: "")
+Comment.create(user_id: u5, memory_id: , body: "")
+Comment.create(user_id: u5, memory_id: , body: "")
+Comment.create(user_id: u5, memory_id: , body: "")
+Comment.create(user_id: u6, memory_id: , body: "")
+Comment.create(user_id: u6, memory_id: , body: "")
+Comment.create(user_id: u6, memory_id: , body: "")
+Comment.create(user_id: u7, memory_id: , body: "")
+Comment.create(user_id: u7, memory_id: , body: "")
+Comment.create(user_id: u7, memory_id: , body: "")
+Comment.create(user_id: u8, memory_id: , body: "")
+Comment.create(user_id: u8, memory_id: , body: "")
+Comment.create(user_id: u8, memory_id: , body: "")
+Comment.create(user_id: u9, memory_id: , body: "")
+Comment.create(user_id: u9, memory_id: , body: "")
+Comment.create(user_id: u9, memory_id: , body: "")
+Comment.create(user_id: u10, memory_id: , body: "")
+Comment.create(user_id: u10, memory_id: , body: "")
+Comment.create(user_id: u10, memory_id: , body: "")
+Comment.create(user_id: u11, memory_id: , body: "")
+Comment.create(user_id: u11, memory_id: , body: "")
+Comment.create(user_id: u11, memory_id: , body: "")
+Comment.create(user_id: u12, memory_id: , body: "")
+Comment.create(user_id: u12, memory_id: , body: "")
+Comment.create(user_id: u12, memory_id: , body: "")
+Comment.create(user_id: u13, memory_id: , body: "")
+Comment.create(user_id: u13, memory_id: , body: "")
+Comment.create(user_id: u13, memory_id: , body: "")
+Comment.create(user_id: u14, memory_id: , body: "")
+Comment.create(user_id: u14, memory_id: , body: "")
+Comment.create(user_id: u14, memory_id: , body: "")
+
+
+
 
 puts "grow baby grow 👨🏻‍🌾"
