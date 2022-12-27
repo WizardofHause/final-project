6a2d23d570f45d7a3b8ffabef2d2052550b44cbc
WizardofHause <jonrhause@gmail.com>
2022-12-27 13:51:31 -0700

    minor edits for date display

--- client/src/components/MemoryCard.js
+++ client/src/components/MemoryCard.js
@@ -10,7 +10,7 @@ function MemoryCard({ memory }) {
                     <h4>{category} - {status}</h4>
                     <Link to={`/memories/${memory.id}`} style={{ textDecoration: 'none' }}><img src={main_img} alt="Render Error!" width="200" /></Link>
                     <p>Likes: {likes}</p>
-                    <p>Date: {date}</p>
+                    <p>{date}</p>
                     <p>{description}</p>
                     <Link to={`/memories/${memory.id}`} style={{ textDecoration: 'none' }}><button>More Details!</button></Link>
                 <span className="circle" />
