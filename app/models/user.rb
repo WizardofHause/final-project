18f93bf7c3496c36a6873f85dc14ff1d23d11526
WizardofHause <jonrhause@gmail.com>
2022-12-31 12:48:17 -0700

    set up validations for new user create

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,11 @@ class User < ApplicationRecord
     
     has_secure_password
 
-    # validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-    # validates :name, presence: true, on: :create
-    # validates :name, uniqueness: true, on: :create
-    # validates :name, length: { maximum: 30 }, on: :create
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+    validates :name, presence: true, on: :create
+    validates :name, uniqueness: true, on: :create
+    validates :name, length: { maximum: 30 }, on: :create
+    validates :password, length: { minimum: 5 }, on: :create
     # validates :password, confirmation: true, length: { minimum: 5 }, on: :create
     # validates :password_confirmation, presence: true
 end
