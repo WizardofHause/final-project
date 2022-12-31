ac64a20edf34f294922daa6b17efd50a4228e934
WizardofHause <jonrhause@gmail.com>
2022-12-31 15:51:14 -0700

    removed index number from error message display

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -65,7 +65,7 @@ function SignUp({ updateUser }) {
 
                 <input type='submit' value='Sign up!' />
             </form>
-            {errors ? errors.map(e => <div>{e[0] + ': ' + e[1]}</div>) : null}
+            {errors ? errors.map(e => <div>{e[1]}</div>) : null}
         </>
     )
 }
