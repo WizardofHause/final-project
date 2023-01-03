d0bcf3861a64e32cf384077849f7777afa439392
WizardofHause <jonrhause@gmail.com>
2023-01-03 14:18:23 -0700

    turned the entire memory card into a link to details

--- client/src/components/MemoryCard.js
+++ client/src/components/MemoryCard.js
@@ -5,15 +5,18 @@ function MemoryCard({ memory }) {
     return (
         <div className='tl-item'>
             <div className='tl-content'>
+            <Link to={`/memories/${memory.id}`} style={{ textDecoration: 'none' }}>
                 <span className='tag'/>
                     <h1>{title} by {user.name}</h1>
                     <h4>{category} - {status}</h4>
-                    <Link to={`/memories/${memory.id}`} style={{ textDecoration: 'none' }}><img src={main_img} alt="Render Error!" width="200" /></Link>
+                    {/* <Link to={`/memories/${memory.id}`} style={{ textDecoration: 'none' }}><img src={main_img} alt="Render Error!" width="200" /></Link> */}
+                    <img src={main_img} alt="Render Error!" width="200" />
                     <p>Likes: {likes}</p>
                     <p>{date}</p>
                     <p>{description}</p>
-                    <Link to={`/memories/${memory.id}`} style={{ textDecoration: 'none' }}><button>More Details!</button></Link>
+                    {/* <Link to={`/memories/${memory.id}`} style={{ textDecoration: 'none' }}><button>More Details!</button></Link> */}
                 <span className="circle" />
+                </Link>
             </div>
         </div>
     )
