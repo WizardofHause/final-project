ba1a32be30f5ae7672fda4ac82de9856aa84dac7
WizardofHause <jonrhause@gmail.com>
2023-01-04 22:49:52 -0700

    got dates to format correctly

--- client/src/components/MemoryCard.js
+++ client/src/components/MemoryCard.js
@@ -1,9 +1,15 @@
 import { Link } from 'react-router-dom'
+import { format } from 'date-fns'
 
 function MemoryCard({ memory }) {
     const { title, category, status, main_img, description, likes, user, date } = memory
     console.log(date)
-
+    // const newDate = (date) => {
+    //     const dateArray = []
+    //     dateArray.push(`${date}T00:00`)
+    //     return dateArray[0]
+    // }
+    // console.log(newDate(date))
     return (
         <div className='tl-item'>
             <div className='tl-content'>
@@ -14,7 +20,8 @@ function MemoryCard({ memory }) {
                     {/* <Link to={`/memories/${memory.id}`} style={{ textDecoration: 'none' }}><img src={main_img} alt="Render Error!" width="200" /></Link> */}
                     <img src={main_img} alt="Render Error!" width="200" />
                     <p>Favorites: {likes}</p>
-                    <p>{date}</p>
+                    {/* <p>{date}</p> */}
+                    <p>{format(new Date(date), 'MMMM d, yyyy (EEEE)')}</p>
                     <p>{description}</p>
                     {/* <Link to={`/memories/${memory.id}`} style={{ textDecoration: 'none' }}><button>More Details!</button></Link> */}
                 <span className="circle" />
