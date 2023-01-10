9bab96d08d734b45f2cc59b4eaf71e80e3e53879
WizardofHause <jonrhause@gmail.com>
2023-01-10 09:24:47 -0700

    minor edit to profile card

--- client/src/components/ProfileCard.js
+++ client/src/components/ProfileCard.js
@@ -11,12 +11,12 @@ export default function ProfileCard({ user_profile }) {
     // add conditional rendering if certain fields aren't filled out
     return (
         <div class='bg-stone-100 border-4 border-stone-300 rounded-lg p-6 text-stone-600 font-semibold ml-44'>
-            <p>Full Name {first_name} {last_name}</p>
-            <p>Born {format(new Date(actualDate(dob)), 'EEEE: MMMM d, yyyy')}</p> 
-            <p>From {pob}</p>
-            <p>Current City: {current_city}</p>
-            <p>Occupation: {family}</p>
-            <p>User Bio: {interests}</p>
+            <p>Full Name: <span class='italic font-light'>{first_name} {last_name}</span></p>
+            <p>Born: <span class='italic font-light'>{format(new Date(actualDate(dob)), 'EEEE: MMMM d, yyyy')}</span></p> 
+            <p>From: <span class='italic font-light'>{pob}</span></p>
+            <p>Current City: <span class='italic font-light'>{current_city}</span></p>
+            <p>Occupation: <span class='italic font-light'>{family}</span></p>
+            <p>User Bio: <span class='italic font-light'>{interests}</span></p>
         </div>
     )
 }
